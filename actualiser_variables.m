94edcd58e248de663f5a626afeec83b56365d839
Targamas <jonathan.cr1302@gmail.com>
2015-11-21 00:35:29 +0100

    Ajout frottement visqueux

--- actualiser_variables.m
+++ actualiser_variables.m
@@ -1,17 +1,18 @@
 %script actualiser_variables
 echo off
 clear
-global k m1 m2 l l0 d A omega  %Initialisation paramètres 
+global k m1 m2 l l0 d A omega c  %Initialisation paramètres 
 
 %Valeurs des paramètres
-k=20;
+k=0;
 l=10;
 l0=10;
 d=30;
 m1=50;
 m2=5;
-A=20;
+A=0;
 omega=20;  
+c = 200;
 t=[0:0.2:40];
 x0=[pi/2;0;0;0];  % CI : angle1 , vitesse angulaire 1, angle2, vitesse angulaire2 
 
