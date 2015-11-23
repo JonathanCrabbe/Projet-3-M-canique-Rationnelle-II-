c0ac247dfe1b15bf50bb9ba7da5b03d975081d32
Targamas <jonathan.cr1302@gmail.com>
2015-11-23 23:23:44 +0100

    Corrections

--- actualiser_variables.m
+++ actualiser_variables.m
@@ -4,19 +4,19 @@ clear
 global k m1 m2 l l0 d A omega c  %Initialisation paramètres 
 
 %Valeurs des paramètres
-k=0;
+k=4;
 l=10;
 l0=10;
 d=30;
-m1=50;
+m1=12;
 m2=5;
 A=0;
 omega=20;  
-c = 200;
+c = 2;
 t=[0:0.2:40];
 x0=[pi/2;0;0;0];  % CI : angle1 , vitesse angulaire 1, angle2, vitesse angulaire2 
 
 fin = columns(t);
-ech = 20;
+ech = 2;
 
 save variables.mat  %Stocke les variables dans un fichier
--- diagramme_bifurcation.m
+++ diagramme_bifurcation.m
@@ -5,13 +5,13 @@ load variables.mat
 
 
 fig1=figure('Name','Diagrammes de bifurcations - Double Pendule');
-fig2=figure('Name','Diagrammes de bifurcations - Double Pendule - CI: (0,0)');
-fig3=figure('Name','Diagrammes de bifurcations - Double Pendule - CI: (0,1/1000)');
-fig4=figure('Name','Diagrammes de bifurcations - Double Pendule - CI: (0,2/1000)');
+%fig2=figure('Name','Diagrammes de bifurcations - Double Pendule - CI: (0,0)');
+%fig3=figure('Name','Diagrammes de bifurcations - Double Pendule - CI: (0,1/1000)');
+%fig4=figure('Name','Diagrammes de bifurcations - Double Pendule - CI: (0,2/1000)');
 
  
 for parametre=0.9:0.01:2  %Indiquer les valeurs du paramètre à moduler ici
-   k = parametre %Indiquer le paramètre à moduler ici
+   A = parametre %Indiquer le paramètre à moduler ici
    for i=0:2
    i
    clear theta
@@ -19,40 +19,44 @@ for parametre=0.9:0.01:2  %Indiquer les valeurs du paramètre à moduler ici
   
    if i==0
       figure(fig1)
-      plot(parametre,theta(1:ech:fin,2),'rs','Markersize',2);
-      hold on
-      figure(fig2)
-      plot(parametre,theta(1:ech:fin,2),'rs','Markersize',2)
-      hold on
+      plot(parametre,theta(1:ech:fin,2), 'Color', 'r','Marker','s' ,'Markersize',2);
+      hold on; 
+      %figure(fig2)
+      %plot(parametre,theta(1:ech:fin,2),'rs','Markersize',2)
+      %hold on
    end
    
    if i==1
       figure(fig1)
-      plot(parametre,theta(1:ech:fin,2),'bs','Markersize',2);
-      hold on
-      figure(fig3)
-      plot(parametre,theta(1:ech:fin,2),'bs','Markersize',2)
-      hold on
+      plot(parametre,theta(1:ech:fin,2), 'Color','b','Marker','s','Markersize',2);
+      hold on; 
+
+      %figure(fig3)
+      %plot(parametre,theta(1:ech:fin,2),'bs','Markersize',2)
+      %hold on
    end
   
    if i==2
       figure(fig1)
-      plot(parametre,theta(1:ech:fin,2),'gs','Markersize',2);
-      hold on
-      figure(fig4)
-      plot(parametre,theta(1:ech:fin,2),'gs','Markersize',2)
-      hold on
+      plot(parametre,theta(1:ech:fin,2), 'Color','g','Marker','s','Markersize',2);
+      hold on; 
+      %figure(fig4)
+      %plot(parametre,theta(1:ech:fin,2),'gs','Markersize',2)
+      %hold on
    end
    end
 end    
    
 figure(fig1);
 axis([0.9 2 -1 3])
-title('Diagramme de bifurcation du double pendule - paramètre: k');
-xlabel('k');
+title('Diagramme de bifurcation du double pendule - paramètre: A');
+xlabel('A');
 ylabel('d\theta1/dt');
  
-figure(fig2);
+
+#{
+ 
+ figure(fig2);
 axis([0.9 2 -1 3])
 title('Diagramme de bifurcation du double pendule - paramètre: k');
 xlabel('k');
@@ -69,5 +73,8 @@ axis([0.9 2 -1 3])
 title('Diagramme de bifurcation du double pendule - paramètre: k');
 xlabel('k');
 ylabel('d\theta1/dt'); 
+
+#}
+
 
   
